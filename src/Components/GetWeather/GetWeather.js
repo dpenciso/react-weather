df8a69581c9af85f13c9b3286795c55740c47b16
David Enciso <dpenciso@msn.com>
2021-03-17 17:29:33 -0600

    hid key

--- src/Components/GetWeather/GetWeather.js
+++ src/Components/GetWeather/GetWeather.js
@@ -1,11 +1,14 @@
+import React from "react";
 import axios from "axios";
+
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 
 const options = {
   method: "GET",
   url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
   params: { q: "Orem", days: "3" },
   headers: {
-    "x-rapidapi-key": "1ec5386572msh7c7a51b410e32c2p16d50fjsndaa0d8e46542",
+    "x-rapidapi-key": API_KEY,
     "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
   },
 };
@@ -20,7 +23,7 @@ const returnWeather = async () => {
       console.error(error);
     });
   console.log(response);
-}
+};
 
 const GetWeather = () => {
   return (
